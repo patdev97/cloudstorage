12c703946d8d08e4fbe27b70a70e959f6dd33e7e
Patrick Pontow <patrick.pontow@gmail.com>
2021-06-26 20:19:56 +0200

    Finalized testing procedure according to rubric items

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
@@ -40,6 +41,7 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
+	@Order(1)
 	public void unauthorizedUser() {
 		driver.get(BASE_URL + this.port + "/login");
 		Assertions.assertEquals("Login", driver.getTitle());
@@ -55,10 +57,13 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
-	public void signupLoginHomeLogout() {
-		this.signupAndLogin();
+	@Order(2)
+	public void signupLoginHomeLogout() throws InterruptedException {
+		this.signupAndLogin(false);
 
 		Assertions.assertEquals("Home", driver.getTitle());
+
+		//Thread.sleep(360000);
 
 		HomePage homePage = new HomePage(driver);
 		homePage.logout();
@@ -70,23 +75,21 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("Login", driver.getTitle());
 	}
 
-	private void signupAndLogin() {
-		driver.get(BASE_URL + this.port + "/signup");
-
-		SignupPage signupPage = new SignupPage(driver);
-		signupPage.signup("Hans", "Wurst", "testuser", "testpass");
-
+	private void signupAndLogin(boolean loginOnly) {
+		if(!loginOnly) {
+			driver.get(BASE_URL + this.port + "/signup");
+			SignupPage signupPage = new SignupPage(driver);
+			signupPage.signup("Hans", "Wurst", "testuser", "testpass");
+		}
 		driver.get(BASE_URL + this.port + "/login");
-
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.login("testuser", "testpass");
 	}
 
 	@Test
-	public void notes() throws InterruptedException {
-		this.signupAndLogin();
-
-		// CREATE Test
+	@Order(3)
+	public void createNote() throws InterruptedException {
+		this.signupAndLogin(true);
 
 		HomePage homePage = new HomePage(driver);
 		homePage.navNoteAndAdd();
@@ -102,28 +105,45 @@ class CloudStorageApplicationTests {
 
 		Assertions.assertEquals("Test title", notes[0].get(0));
 		Assertions.assertEquals("Test Note. Wanting to know if it works.", notes[1].get(0));
+	}
 
-		// EDIT Test
+	@Test
+	@Order(4)
+	public void editNote() throws InterruptedException {
+		this.signupAndLogin(true);
 
+		HomePage homePage = new HomePage(driver);
+		homePage.navNotesTab.click();
+		Thread.sleep(1000);
 		homePage.clickEditNote(driver, 0);
-		Thread.sleep(200);
+		Thread.sleep(1000);
 
 		homePage.createNote("New title", "And also a new description");
 
+		ResultPage resultPage = new ResultPage(driver);
 		resultPage.link.click();
 
 		homePage.navNotesTab.click();
 		Thread.sleep(1000);
 
-		notes = homePage.getNotes(driver);
+		List[] notes = homePage.getNotes(driver);
 
 		Assertions.assertEquals("New title", notes[0].get(0));
 		Assertions.assertEquals("And also a new description", notes[1].get(0));
+	}
 
-		// DELETE Test
+	@Test
+	@Order(5)
+	public void deleteNote() throws InterruptedException {
+		this.signupAndLogin(true);
+
+		HomePage homePage = new HomePage(driver);
+		homePage.navNotesTab.click();
+		Thread.sleep(1000);
 
 		homePage.deleteNote(driver, 0);
 
+		ResultPage resultPage = new ResultPage(driver);
 		resultPage.link.click();
 
 		homePage.navNotesTab.click();
@@ -141,10 +161,9 @@ class CloudStorageApplicationTests {
 	}
 
 	@Test
-	public void credentials() throws InterruptedException {
-		this.signupAndLogin();
-
-		// CREATE Test
+	@Order(6)
+	public void createCredential() throws InterruptedException {
+		this.signupAndLogin(true);
 
 		HomePage homePage = new HomePage(driver);
 		homePage.navCredentialsAndAdd();
@@ -161,14 +180,25 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("google.com", credentials.get(0)[2].getText());
 		Assertions.assertEquals("testuser", credentials.get(0)[3].getText());
 		Assertions.assertNotEquals("pass", credentials.get(0)[4].getText());
+	}
 
-		// EDIT Test
+	@Test
+	@Order(7)
+	public void editCredential() throws InterruptedException {
+		this.signupAndLogin(true);
+
+		HomePage homePage = new HomePage(driver);
+		homePage.navCredentialsTab.click();
+		Thread.sleep(1000);
+
+		List<WebElement[]> credentials = homePage.getCredentials(driver);
 
 		credentials.get(0)[0].click();
 		Thread.sleep(1000);
 
 		homePage.createCredential("bing.com", "newuser","newpass");
 
+		ResultPage resultPage = new ResultPage(driver);
 		resultPage.link.click();
 
 		homePage.navCredentialsTab.click();
@@ -179,11 +209,22 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("bing.com", credentials.get(0)[2].getText());
 		Assertions.assertEquals("newuser", credentials.get(0)[3].getText());
 		Assertions.assertNotEquals("newpass", credentials.get(0)[4].getText());
+	}
 
-		// DELETE Test
+	@Test
+	@Order(8)
+	public void deleteCredential() throws InterruptedException {
+		this.signupAndLogin(true);
+
+		HomePage homePage = new HomePage(driver);
+		homePage.navCredentialsTab.click();
+		Thread.sleep(1000);
+
+		List<WebElement[]> credentials = homePage.getCredentials(driver);
 
 		credentials.get(0)[1].click();
 
+		ResultPage resultPage = new ResultPage(driver);
 		resultPage.link.click();
 
 		homePage.navCredentialsTab.click();
